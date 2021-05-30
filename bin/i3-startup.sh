02e80470d567d4680a289685638644242c0bb63d
Paul Musgrave <ps.musgrave@gmail.com>
2021-05-29 20:07:42 -0400

    Fix i3 layout after emacs update

--- bin/i3-startup.sh
+++ bin/i3-startup.sh
@@ -4,4 +4,4 @@ sleep 2
 i3-resurrect restore -w 1 -d /home/paul/.config/i3
 i3-resurrect restore -w 2 -d /home/paul/.config/i3
 i3-resurrect restore -w 4 -d /home/paul/.config/i3
-i3-resurrect restore -w 5 -d /home/paul/.config/i3
+#i3-resurrect restore -w 5 -d /home/paul/.config/i3
